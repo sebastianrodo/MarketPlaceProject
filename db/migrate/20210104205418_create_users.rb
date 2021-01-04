1a7b47bf95cc2fd64dc7b4f82e9e0a3eb5cbff78
sebastianrodo <sebastian.rodriguez@koombea.com>
2021-01-04 16:01:16 -0500

    Create a User Model

---
+++ db/migrate/20210104205418_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string  :first_name, null: false
+      t.string  :last_name,  null: false
+      t.string :email,       null: false
+      t.integer :cellphone,  default: ""
+      t.string  :address,    default: ""
+
+      t.timestamps
+    end
+  end
+end
