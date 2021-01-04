34a340588c2eee22649c859b74d2589ace453a58
sebastianrodo <sebastian.rodriguez@koombea.com>
2021-01-04 17:28:18 -0500

    Create view / controller to list products

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Object successfully created"
+      flash[:success] = "User successfully created"
       redirect_to users_url
     else
       flash[:error] = "Something went wrong"
