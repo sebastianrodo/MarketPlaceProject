493d49eb3774123084857b961cbe071b5f0b1c82
sebastianrodo <sebastian.rodriguez@koombea.com>
2021-02-26 08:39:53 -0500

    Update user migration, (change cellphone and address)

--- db/migrate/20210104205418_create_users.rb
+++ db/migrate/20210104205418_create_users.rb
@@ -4,8 +4,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string  :first_name, null: false
       t.string  :last_name,  null: false
       t.string :email,       null: false
-      t.integer :cellphone,  default: ""
-      t.string  :address,    default: ""
+      t.bigint :cellphone,   null: true, limit: 10
+      t.string  :address
 
       t.timestamps
     end
